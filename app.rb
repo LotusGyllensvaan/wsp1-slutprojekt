c6ab9b2ee4cdb9aae1915736d51ec8b702db70fd
LotusGyllensvaan <lotus.gyllensvaan@elev.ga.ntig.se>
2025-02-22 13:27:28 +0100

    Login

--- app.rb
+++ app.rb
@@ -2,19 +2,43 @@ class App < Sinatra::Base
 
     get '/' do
         redirect("/home")
+        if session[:user_id]
+            erb(:"admin/index")
+        else
+            erb :index
+        end
     end
 
     def db
         return @db if @db
 
-        @db = SQLite3::Database.new("db/equipment.sqlite")
+        @db = SQLite3::Database.new("db/database.sqlite")
         @db.results_as_hash = true
         
         return @db
     end
 
+    configure do
+        enable :sessions
+        set :session_secret, SecureRandom.hex(64)
+    end
+
+    get '/admin' do
+        if session[:user_id]
+          erb(:"admin/index")
+        else
+          p "/admin : Access denied."
+          status 401
+          redirect '/unauthorized'
+        end
+    end
+
+    get '/unauthorized' do
+        erb(:"unauthorized")
+    end
+
     get '/home' do
-        p @products = db.execute('SELECT * FROM equipment')
+        @products = db.execute('SELECT * FROM equipment')
         erb(:"index")
     end
 
@@ -22,4 +46,53 @@ class App < Sinatra::Base
         erb(:"login")
     end
 
-end
+    post '/login' do
+        request_username = params[:username]
+        request_plain_password = params[:password]
+    
+        user = db.execute("SELECT *
+                FROM users
+                WHERE username = ?",
+                request_username).first
+    
+        unless user
+          p "/login : Invalid username."
+          status 401
+          redirect '/unauthorized'
+        end
+    
+        db_id = user["id"].to_i
+        db_password_hashed = user["password"].to_s
+    
+        # Create a BCrypt object from the hashed password from db
+        bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+        # Check if the plain password matches the hashed password from db
+        if bcrypt_db_password == request_plain_password
+          session[:user_id] = db_id
+          redirect '/admin'
+        else
+          status 401
+          redirect '/unauthorized'
+        end
+    
+      end
+
+      get '/signup' do
+        erb(:"signup")
+      end
+
+      post '/signup' do
+        input_username = params[:username]
+        password_hashed = BCrypt::Password.create(params[:password])
+
+        db.execute("INSERT INTO users (email, username, password) VALUES(?,?,?)", 
+        [
+            params['email'], 
+            params['username'],
+            password_hashed
+        ])
+
+        redirect '/home'
+      end
+
+    end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'bcrypt'
 
 class Seeder
 
@@ -10,6 +11,7 @@ class Seeder
 
   def self.drop_tables
     db.execute('DROP TABLE IF EXISTS equipment')
+    db.execute('DROP TABLE IF EXISTS users')
   end
 
   def self.create_tables
@@ -18,20 +20,29 @@ class Seeder
                 article TEXT NOT NULL,
                 description TEXT NOT NULL,
                 category TEXT NOT NULL)')
+
+    db.execute('CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL)')
+    
   end
 
   def self.populate_tables
-    
     db.execute('INSERT INTO equipment (article, description, category) VALUES ("Fishing Rod", "High quality rod used for the most damndest of fish.", "Rods")')
     db.execute('INSERT INTO equipment (article, description, category) VALUES ("Tackle Box", "Watch how I tackle box", "Storage")')
     db.execute('INSERT INTO equipment (article, description, category) VALUES ("Fishing Line", "Withstand a damn behemoth", "Line")')
     db.execute('INSERT INTO equipment (article, description, category) VALUES ("Grubs", "German Assasinations", "Bait")')
+
+    password_hashed = BCrypt::Password.create("123")
+    db.execute('INSERT INTO users (email, username, password) VALUES (?, ?, ?)', ["lotus.gyllensvaan@gmail.com", "admin", password_hashed])
   end
 
   private
   def self.db
     return @db if @db
-    @db = SQLite3::Database.new('db/equipment.sqlite')
+    @db = SQLite3::Database.new('db/database.sqlite')
     @db.results_as_hash = true
     @db
   end
